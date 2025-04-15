80de6333f18aece5efa53cc2618998f4c8395c3e
britojulia <juliabri1234@gmail.com>
2025-04-15 10:27:40 -0300

    first commit

---
+++ FeudoMarket/src/main/java/br/com/fiap/FeudoMarket/FeudoMarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FeudoMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeudoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeudoMarketApplication.class, args);
+	}
+
+}
